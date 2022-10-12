1573aa15a423ace3152b19b23bb4568cbdcb431c
Marek Fišera <mara@neptuo.com>
2022-10-12 09:50:55 +0200

    Debounce user input.

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,15 +1,17 @@
 import { QrImage } from 'qrlibrary';
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
+import debounce from 'lodash.debounce';
 
 function App() {
   const [text, setText] = useState("Hello from react!");
+  const debouncedSetText = useCallback(debounce(setText, 2000), []);
 
   return (
     <>
       <div>
         Generate a QR from text: 
         <br />
-        <input type="text" placeholder="Hello from react!" onChange={e => setText(e.target.value)} />
+        <input type="text" placeholder="Hello from react!" onChange={e => debouncedSetText(e.target.value)} />
       </div>
       <div>
         <QrImage text={text} />
