837913b09006905ff35902278fa0f4803a76e56c
Marek Fišera <mara@neptuo.com>
2022-10-12 09:44:30 +0200

    Now it works, the only thing is that microbundler drops the webpackIgnore comment.

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,10 +1,18 @@
 import { QrImage } from 'qrlibrary';
+import { useState } from 'react';
 
 function App() {
+  const [text, setText] = useState("Hello from react!");
+
   return (
     <>
       <div>
-        <QrImage text="Hello from react!" />
+        Generate a QR from text: 
+        <br />
+        <input type="text" placeholder="Hello from react!" onChange={e => setText(e.target.value)} />
+      </div>
+      <div>
+        <QrImage text={text} />
       </div>
     </>
   );
